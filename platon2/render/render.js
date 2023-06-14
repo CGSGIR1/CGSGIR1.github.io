ea0a87a4d8be33a0773c8a4d6c7b33bb9996f9ac
Родин Илья <56633518+CGSGIR1@users.noreply.github.com>
2023-06-14 17:46:08 +0300

    Add files via upload

---
+++ platon2/render/render.js
@@ -0,0 +1,222 @@
+import {
+  prim,
+  primDraw,
+  vert,
+  VertToArray,
+  arrayVert,
+  PrimLoad,
+} from "./prim.js";
+import { vec3, mat4, camera, MatrIdentity, MatrMulMatr } from "./mth/mth.js";
+export { vec3 };
+
+async function loadShaderAsync(shaderName) {
+  try {
+    const response = await fetch(shaderName);
+    const text = await response.text();
+    return text;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function loadShader(gl, type, source) {
+  const shader = gl.createShader(type);
+
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    let buf = gl.getShaderInfoLog(shader);
+    console.log(buf);
+  }
+  return shader;
+}
+
+export function initGL() {
+  const canvas = document.getElementById("glCanvas");
+  const gl = canvas.getContext("webgl2");
+
+  gl.clearColor(0.5, 0.0, 0.5, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  /*let dataBuf = [
+    0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0,
+    1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, -1, 0, 0,
+    1, 1, 1, 0, 1, 1, 1, -1, 0, 0, 1, 1, 0, 1, 0, 0, 1, -1, 0, 0, 1, 0, 0, 0, 1,
+    0, 1, -1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0.5, 1, 0.3, 0, 1,
+    0, 0, 1, 0, 1, 1, 0.4, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0.5, 0.4, 0, 1, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, -1, 0, 0, 0, 1, 1, 0, 0, 1, 0, -1, 0, 0, 1, 1, 1, 0, 1, 1,
+    0, -1, 0, 0, 1, 0, 1, 0, 0, 1, 0, -1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1,
+    0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1,
+    0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, -1, 1, 0,
+    0, 1, 0, 0, 1, 0, 0, -1, 1, 0, 1, 1, 0, 1, 1, 0, 0, -1,
+  ];*/
+  let dataBuf0 = [
+    -1, -1, 1, 0, 1, 0, 1, 1, -1, -1, -1, 1, 1, 1, 0, 0, 1, 1, -1, -1, 1, 1, 1,
+    0, 0, 1, 1, 1, -1, -1, 1, -1, 1, 1, 1, 0, 1, 1, -1, -1, 1, -1, 1, 1, 0, 1,
+    1, -1, -1, -1, 1, 1, 1, 0, 1, 1, 1, -1, -1, -1, 1, 1, -1, 1, 0, 1, 1, -1,
+    -1, -1, 1, -1, -1, 1, 1, 0, 1, -1, -1, -1, 1, -1, -1, 1, 0, 1, 1, -1, 1, -1,
+    -1, -1, -1, 0, 0, 1, -1, -1, 1, -1, -1, 1, -1, 1, 1, 0, 1, -1, 1, -1, 1, 1,
+    -1, 1, 0, 1, 1, -1, 1, -1, -1, -1, -1, 1, 0, 1, 1, -1, -1, -1, -1, -1, 1, 0,
+    0, 1, 1, -1, -1, -1, -1, 1, 1, 1, 0, 1, 1, -1, -1, -1, -1, 1, -1, 1, 0, 1,
+    1, -1, -1, -1, -1, 1, 1, 0, 1, 1, 1, -1, -1, 1, -1, 1, -1, 1, 1, 1, 1, -1,
+    -1, 1, 1, 1, -1, 1, 0, 0, 1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1,
+    -1, 1, 0, 0, 1, 1, -1, -1, -1, -1, -1, -1, 0, 1, 0, 1, -1, -1, -1, 1, -1,
+    -1, 1, 1, 1, 1, -1, -1, -1, 1, -1, 1, 1, 0, 1, 1, -1, -1, -1,
+  ];
+
+  let ind0 = [
+    0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14,
+    15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
+  ];
+
+  let dataBuf = [
+    1, 2, 1, 1, 0, 0, 1, 1, 2, 1, 0, 1, 2, 0, 1, 0, 1, 0, 1, 2, 0, 1, 0, 0, 0,
+    1, 1, 0, 1, 0, 2, 1, 0, 0, 1, 0, 1, 2, 1, 0, 2, 1, 2, 1, 0, 0, 1, 2, 1, 2,
+    1, 0, 1, 0, 1, 0, 1, 1, 0, 1,
+  ];
+
+  let ind = [
+    0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 1, 4, 1, 4, 5, 3, 4, 5, 2, 3, 5, 1, 2, 5,
+  ];
+
+  let dataBuf1 = [
+    1, 2, 1, 1, 1, 0, 1, 1, 2, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 2, 0, 0, 1, 0,
+    1, 1, 2, 0, 0, 1, 0, 2, 0, 1, 0, 1, 1, 0, 2,
+  ];
+
+  let ind1 = [0, 1, 2, 0, 2, 3, 0, 1, 3, 1, 2, 3];
+
+  let dataBuf2 = [
+    0, -1, 0, 0, 1, 0, 1, 1, 0, 0, -0.276, -0.447, -0.851, 1, 0, 0, 1, 1, 0, 0,
+    0.724, -0.447, -0.526, 1, 1, 0, 1, 1, 0, 0, 0.724, -0.447, 0.526, 1, 0, 0,
+    1, -1, 0, 0, -0.276, -0.447, 0.851, 0, 1, 0, 1, -1, 0, 0, -0.894, -0.447, 0,
+    1, 1, 0.4, 1, 0, 1, 0, -0.724, 0.447, 0.526, 1, 1, 0.5, 0.4, 0, 1, 0,
+    -0.724, 0.447, -0.526, 0, 0, 1, 1, 1, 0, 0, 0.276, 0.447, -0.851, 1, 0, 1,
+    1, -1, 0, 0, 0.894, 0.447, 0, 0, 1, 1, 1, -1, 0, 0, 0.276, 0.447, 0.851, 1,
+    0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0.5, 1, 0.3, 0, 1, 0,
+  ];
+
+  let ind2 = [
+    0, 1, 2, 0, 1, 5, 0, 4, 5, 0, 3, 4, 0, 2, 3, 6, 10, 11, 6, 7, 11, 7, 8, 11,
+    8, 9, 11, 9, 10, 11, 2, 3, 9, 1, 2, 8, 1, 5, 7, 4, 5, 6, 3, 4, 10, 4, 6, 10,
+    5, 6, 7, 1, 7, 8, 2, 8, 9, 3, 9, 10,
+  ];
+
+  let dataBuf3 = [
+    0.149, -0.631, -0.459, 0, 1, 0, 1, 1, 0, 0, -0.39, -0.631, -0.284, 1, 0, 0,
+    1, 1, 0, 0, -0.39, -0.631, 0.284, 1, 1, 0, 1, 1, 0, 0, 0.149, -0.631, 0.459,
+    1, 0, 0, 1, -1, 0, 0, 0.483, -0.631, 0, 0, 1, 0, 1, -1, 0, 0, -0.149, 0.631,
+    0.459, 1, 1, 0.4, 1, 0, 1, 0, -0.483, 0.631, 0, 1, 1, 0.5, 0.4, 0, 1, 0,
+    -0.149, 0.631, -0.459, 0, 0, 1, 1, 0, 1, 0, 0.39, 0.631, -0.284, 1, 1, 0, 1,
+    0, 1, 0, 0.39, 0.631, 0.284, 1, 0, 1, 1, 0, 1, 0, 0.781, -0.149, 0, 1, 1, 0,
+    1, 0, 1, 0, 0.241, -0.149, -0.743, 1, 0, 1, 1, 0, 1, 0, -0.631, -0.149,
+    -0.459, 0, 1, 0, 1, 0, 1, 0, -0.631, -0.149, 0.459, 1, 1, 0, 1, 0, 1, 0,
+    0.241, -0.149, 0.743, 1, 0, 1, 1, 0, 1, 0, -0.241, 0.149, 0.743, 1, 1, 0, 1,
+    0, 1, 0, -0.781, 0.149, 0, 0, 1, 1, 1, 0, 1, 0, -0.241, 0.149, -0.743, 1, 0,
+    1, 1, 0, 1, 0, 0.631, 0.149, -0.459, 0, 1, 1, 1, 0, 1, 0, 0.631, 0.149,
+    0.459, 0, 0, 1, 1, 0, 1, 0,
+  ];
+
+  let ind3 = [
+    0, 1, 2, 0, 2, 3, 0, 3, 4, 5, 6, 7, 5, 7, 9, 7, 8, 9, 5, 9, 15, 9, 14, 15,
+    9, 14, 19, 2, 13, 15, 2, 3, 15, 3, 14, 15, 5, 6, 15, 6, 15, 16, 13, 15, 16,
+    3, 4, 14, 4, 14, 19, 4, 10, 19, 1, 2, 13, 1, 12, 13, 12, 13, 16, 7, 8, 17,
+    8, 11, 17, 8, 11, 18, 10, 11, 18, 4, 10, 11, 0, 4, 11, 8, 9, 19, 8, 18, 19,
+    10, 18, 19, 0, 1, 11, 1, 11, 17, 1, 12, 17, 12, 16, 17, 6, 16, 17, 6, 7, 17,
+  ];
+
+  const vs = loadShaderAsync("./render/vert.vert");
+  const fs = loadShaderAsync("./render/frag.frag");
+
+  Promise.all([vs, fs]).then((res) => {
+    const vertexSh = loadShader(gl, gl.VERTEX_SHADER, res[0]);
+    const fragmentSh = loadShader(gl, gl.FRAGMENT_SHADER, res[1]);
+
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexSh);
+    gl.attachShader(shaderProgram, fragmentSh);
+    gl.linkProgram(shaderProgram);
+
+    const vBuf = gl.createBuffer();
+
+    let cube = prim(gl, gl.TRIANGLES, dataBuf0, 24, ind0, shaderProgram);
+    let cam1 = camera();
+
+    //  const dataBuf = [-1, -1, 0, 1, 1, 1, 0, 1,
+    //                   -1, 1, 0, 1,  0, 1, 1, 1,
+    //                     Math.Math.sqrt(3) / 2, 0, 0, 1, 1, 0, 1, 1];
+
+    let tetra = prim(gl, gl.TRIANGLES, dataBuf, 24, ind, shaderProgram);
+    let octa = prim(gl, gl.TRIANGLES, dataBuf1, 24, ind1, shaderProgram);
+    let icos = prim(gl, gl.TRIANGLES, dataBuf2, 24, ind2, shaderProgram);
+    let dode = prim(gl, gl.TRIANGLES, dataBuf3, 24, ind3, shaderProgram);
+
+    const render = () => {
+      //    gl.bindBuffer(gl.ARRAY_BUFFER, vBuf);
+      //    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(dataBuf), gl.STATIC_DRAW);
+
+      //    gl.vertexAttribPointer(0, 4, gl.FLOAT, false, 8 * 4, 0);
+      //    gl.vertexAttribPointer(1, 4, gl.FLOAT, false, 8 * 4, 4 * 4);
+
+      //    gl.enableVertexAttribArray(0);
+      //    gl.enableVertexAttribArray(1);
+
+      //    gl.useProgram(shaderProgram);
+
+      //    gl.drawArrays(gl.TRIANGLE_STRIP, 0, dataBuf.length / 8);]
+      //console.log(prim1);
+      gl.enable(gl.DEPTH_TEST);
+      // primDraw(
+      //  prim1,
+      //  cam1,
+      //  mat4().setRotate(Math.sin(Date.now() / 1000.0), vec3(1, 2, 3))
+      // );
+
+      primDraw(
+        cube,
+        cam1,
+        MatrMulMatr(
+          MatrMulMatr(
+            mat4().MatrTranslate(vec3(-1, 1, 0.5)),
+            mat4().setRotate(Math.sin(Date.now() / 2373.0), vec3(1, 1, 1))
+          ),
+          mat4().MatrScale(vec3(0.5, 0.5, 0.5))
+        )
+      );
+      primDraw(
+        tetra,
+        cam1,
+        mat4().setRotate(Math.sin(Date.now() / 787.0), vec3(2, 4, 3))
+      );
+      primDraw(
+        octa,
+        cam1,
+        MatrMulMatr(
+          mat4().MatrTranslate(vec3(1, 2, 1)),
+          mat4().setRotate(Math.sin(Date.now() / 1123.0), vec3(2, 2, 3))
+        )
+      );
+      primDraw(
+        icos,
+        cam1,
+        MatrMulMatr(
+          mat4().MatrTranslate(vec3(1, 0.5, 4)),
+          mat4().setRotate(Math.sin(Date.now() / 1467.0), vec3(0.5, 1, 1))
+        )
+      );
+      primDraw(
+        dode,
+        cam1,
+        MatrMulMatr(
+          mat4().MatrTranslate(vec3(1, 0.5, 4)),
+          mat4().setRotate(Math.sin(Date.now() / 1783.0), vec3(0.5, 1, 0))
+        )
+      );
+
+      window.requestAnimationFrame(render);
+    };
+
+    render();
+  });
+}
